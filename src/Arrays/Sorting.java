0bf071c7e4a5ad001dffa30ff5a9652f1e9eaf7c
Radha2786 <108953658+Radha2786@users.noreply.github.com>
2022-09-08 16:27:27 +0530

    first commit

---
+++ src/Arrays/Sorting.java
@@ -0,0 +1,10 @@
+package Arrays;
+
+import java.util.*;
+public class Sorting {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println();
+        Arrays.sort();
+    }
+}
