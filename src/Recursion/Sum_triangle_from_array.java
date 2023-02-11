e3171ffe465632c9b783240d698f1d595ec6b9d1
Radha2786 <108953658+Radha2786@users.noreply.github.com>
2023-02-11 23:33:45 +0530

    JAVA QUESTION

--- src/Recursion/Sum_triangle_from_array.java
+++ src/Recursion/Sum_triangle_from_array.java
@@ -5,18 +5,21 @@ import java.util.Arrays;
 public class Sum_triangle_from_array {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
+        int[] temp =arr;
         sum(arr);
+        System.out.println(Arrays.toString(temp));
     }
     static void sum(int[] arr){
+
         if(arr.length==1){
-//            System.out.println(Arrays.toString(arr));
             return;
+
         }
         int[] arr2 = new int[arr.length-1];
         for(int i=0;i<arr.length-1;i++){
             arr2[i]=arr[i]+arr[i+1];
         }
-        System.out.println(Arrays.toString(arr2));
         sum(arr2);
+        System.out.println(Arrays.toString(arr2));
     }
 }
