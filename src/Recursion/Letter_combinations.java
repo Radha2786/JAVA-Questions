920e14282247c7edb9ecdf14ad1d18b3729f0e03
GARY <108233340+GARY121github@users.noreply.github.com>
2023-03-05 17:03:56 +0530

    Permutations

--- src/Recursion/Letter_combinations.java
+++ src/Recursion/Letter_combinations.java
@@ -4,26 +4,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Letter_combinations {
+
+    int a = 10;
     public static void main(String[] args) {
-        String st = "23";
-        List<String> ans = new ArrayList<>();
-        letter_combinations(st , "" , ans);
-        System.out.println(ans);
+        String digits_pressed = "23";
+        ArrayList<String> list = new ArrayList<>();
+        phoneLetterCombinations(digits_pressed , "" , list);
+        System.out.println(list);
     }
 
-    static String[] choices_of_number = {"" , "" , "abc" , "def" , "ghi" , "jkl" , "mno" , "pqrs" , "tvu" , "wxyz"};
+    static String[] digits_choices = { "" , "" , "abc" , "def" , "ghi" , "jkl" , "mno" , "pqrs" , "tuv" , "wxyz"};
 
-    static void letter_combinations(String digits , String ans , List<String> list){
+    static void phoneLetterCombinations(String digits , String ans , ArrayList<String> list){
         if(digits.isEmpty()){
+//            System.out.println(ans);
             list.add(ans);
             return;
         }
+//        -> "23"
+        int digit_pressed = digits.charAt(0) - '0';
 
-        int digit = digits.charAt(0) - '0';
-        String choice = choices_of_number[digit];
+        String choices_present_at_that_digit = digits_choices[digit_pressed];
 
-        for(int i = 0 ; i < choice.length() ; i++){
-            letter_combinations(digits.substring(1) , ans+choice.charAt(i) , list);
+        for(int i = 0 ; i < choices_present_at_that_digit.length() ; i++){
+            char ch = choices_present_at_that_digit.charAt(i);
+            phoneLetterCombinations(digits.substring(1) , ans+ch , list);
         }
     }
 }
--- src/Recursion/Lexicographically_counting.java
+++ src/Recursion/Lexicographically_counting.java
@@ -1,7 +1,36 @@
 package Recursion;
 
+import java.util.ArrayList;
+
 public class Lexicographically_counting {
     public static void main(String[] args) {
+        int n = 13;
+        ArrayList<Integer> list = new ArrayList<>();
+        lexicographicalCounting(n , 0 , list);
+        System.out.println(list);
+    }
+
+    static void lexicographicalCounting(int n , int ans , ArrayList<Integer> list){
+        // BASE CONDITION
+        if(ans > n){
+            return;
+        }
+
+        if(ans != 0){
+//            System.out.println(ans);
+            list.add(ans);
+        }
+
+        int choose = 0;
+
+        if(ans == 0){
+            choose = 1;
+        }
+
+        while(choose <= 9){
+            lexicographicalCounting(n , ans*10 + choose , list);
+            choose++;
+        }
 
     }
 }
