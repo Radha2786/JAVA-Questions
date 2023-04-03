1c1e202295000716d50b3a6a925d02a2b119ac01
Radha Sharma <radhasharma2786@gmail.com>
2023-04-03 16:15:27 +0530

    JAVA Q UESTION

---
+++ src/LEETCODE/Merge_two_sorted_arrays.java
@@ -0,0 +1,35 @@
+package LEETCODE;
+import java.util.*;
+
+public class Merge_two_sorted_arrays {
+    public static void main(String[] args) {
+
+        int[] nums1 = {1,2,3,0,0,0};
+        int m = 3;
+        int[] nums2 = {2,5,6};
+        int n = 3;
+        int[] arr = new int[n+m];
+        int i=0 , j =0,k=0;
+        while(i<m && j <n){
+            if(nums1[i]<nums2[j]){
+                arr[k]=nums1[i];
+                i++;
+            }else{
+                arr[k]=nums2[j];
+                j++;
+            }
+            k++;
+        }
+        while(i<m){
+            arr[k]=nums1[i];
+            i++;
+            k++;
+        }
+        while(j<n){
+            arr[k]=nums2[j];
+            j++;
+            k++;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
