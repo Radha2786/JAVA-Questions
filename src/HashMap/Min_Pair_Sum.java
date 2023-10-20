fbe88166400b9c4fa7921fe9e65cb6767d8ab02c
Radha <radhasharma2786@gmail.com>
2023-10-20 22:41:25 +0530

    min~pair~sum

---
+++ src/HashMap/Min_Pair_Sum.java
@@ -0,0 +1,27 @@
+package HashMap;
+
+// Heap Questions
+import java.util.PriorityQueue;
+
+public class Min_Pair_Sum {
+    public static void main(String[] args) {
+//        int[] arr ={2,3,4,5,6,7,82,3,5,6,1,3};
+        int[] arr ={3,4,2,5,7,11,9};
+        System.out.println(Pair_Sum(arr));
+    }
+    public static int Pair_Sum(int[] arr){
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int i=0;i<arr.length;i++){
+            pq.add(arr[i]);
+        }
+        int sum=0;
+        while(pq.size()>1){
+            int a = pq.poll();
+            int b= pq.poll();
+            sum=a+b;
+            pq.add(a+b);
+        }
+        return sum ;
+    }
+}
